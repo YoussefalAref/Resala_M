d1ea8c52d1ca43807a864f01b98411660670952d
Youssef Al Aref <youssef.ibrahim.8686@gmial.com>
2026-01-07 00:22:52 +0200

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
- 
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import eventRoutes from './routes/events.js';
+import materialRoutes from './routes/materials.js';
+import receiptRoutes from './routes/receipts.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/materials', materialRoutes);
+app.use('/api/receipts', receiptRoutes);
+
+app.listen(process.env.PORT || 5000, () => console.log('Server running...'));
